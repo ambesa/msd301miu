597b0b61b695312a4a08b15b3bfe50d7151582f5
ambesa <ambesa4@gmail.co>
2021-03-10 21:10:57 -0600

    added files

---
+++ w3d3html/index.js
@@ -0,0 +1,11 @@
+// 
+
+
+
+
+
+function fullName() {
+    const fname = document.getElementById("input");
+    const lname = document.getElementById("output");
+    //const output = document.getElementById("output").innerHTML = fname + lname;
+}
